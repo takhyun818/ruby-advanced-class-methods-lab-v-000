498c8ee442c7181c2c0075c8fd81547f2d404f11
Tak H Kim <takhyun818@gmail.com>
2018-11-15 19:44:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,8 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.detect { |artist| artist.name == name }
+    @@all.each do |song|
+      result = song if song.name == name
   end
 
   def self.find_or_create_by_name(name)
